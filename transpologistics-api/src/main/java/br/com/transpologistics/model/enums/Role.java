69ecb15be5d042d1a920342fb213ec93fc744ad3
Pedro Gabriel <92233229+PedroGabriiel@users.noreply.github.com>
2025-10-29 08:33:19 -0300

    Create model/enums

---
+++ transpologistics-api/src/main/java/br/com/transpologistics/model/enums/Role.java
@@ -0,0 +1,7 @@
+package br.com.transpologistics.model.enums;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_MOTORISTA,
+    ROLE_CLIENTE
+}
---
+++ transpologistics-api/src/main/java/br/com/transpologistics/model/enums/StatusEntrega.java
@@ -0,0 +1,8 @@
+package br.com.transpologistics.model.enums;
+
+public enum StatusEntrega {
+    PENDENTE,
+    EM_TRANSITO,
+    ENTREGUE,
+    CANCELADA
+}
